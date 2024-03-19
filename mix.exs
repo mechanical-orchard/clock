d21565e4a763f54287d6c8441bb755022b523475
Jeremy Lightsmith <jeremy.lightsmith@gmail.com>
2024-03-19 16:43:40 -0700

    adding ci stuff

--- mix.exs
+++ mix.exs
@@ -5,24 +5,30 @@ defmodule Clock.MixProject do
     [
       app: :clock,
       version: "0.1.0",
-      elixir: "~> 1.16",
+      elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      dialyzer: [plt_file: {:no_warn, "tmp/plts/dialyzer.plt"}, plt_add_apps: [:mix, :ex_unit]],
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  defp aliases do
+    [
+      ci: ["credo", "dialyzer", "test"]
+    ]
+  end
+
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.2", only: [:dev], runtime: false}
     ]
   end
 end
